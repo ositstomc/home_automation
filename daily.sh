64bc635c5923b09615c49f8bfdea5a1519d409e6
ositstomc <tomc@osits.com>
2018-06-08 14:11:52 +0100

    Create daily.sh
    First scripting of the pyenergenie lights

---
+++ daily.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+w3m "https://www.timeanddate.com/sun/ireland/dublin" | grep "Sunrise Today" | awk '{print $3}' | sed 's/.$//' > /home/pi/sunrise.txt
+w3m "https://www.timeanddate.com/sun/ireland/dublin" | grep "Sunset Today" | awk '{print $3}' | sed 's/.$//' > /home/pi/sunset.txt
+echo "python /home/pi/pyenergenie/src/front_light_on.py" | at `cat /home/pi/sunset.txt`
+echo "python /home/pi/pyenergenie/src/front_light_off.py" | at `cat /home/pi/sunrise.txt`
+sudo find /tmp -iname *.avi -mtime +5 -exec rm -f {} \;
+  
+  
+  
